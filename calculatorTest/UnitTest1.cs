282798e48099361549836eed3802812919a02c63
A00233642 <Taiwoakintunde26@gmail.com>
2021-11-01 23:22:45 -0400

    Test Method for Addition
    A new test method to perform addition operation using 10 different test methods

--- calculatorTest/UnitTest1.cs
+++ calculatorTest/UnitTest1.cs
@@ -10,7 +10,33 @@ namespace calculatorTest
     public class UnitTest1
     {
 
-        
+        // [TestMethod]
+        [DataTestMethod]
+        [DataRow(4, 3, 7)]
+        [DataRow(4, 4, 8)]
+        [DataRow(4, 5, 9)]
+        [DataRow(4, 6, 10)]
+        [DataRow(4, 7, 11)]
+        [DataRow(4, 8, 12)]
+        [DataRow(4, 9, 13)]
+        [DataRow(4, 10, 14)]
+        [DataRow(4, 11, 15)]
+        [DataRow(4, 12, 16)]
+
+
+        [TestCategory("Addition")]
+
+        public void CalculatorAddTestMethod1(double a, double b, double x)
+        {
+
+            double actual = Class1.Add(a, b);
+            Assert.AreEqual(x, actual);
+            // Class1 varCal = new Class1();
+            //  double v = varCal.Add(a, b);
+            // result = Calculator.add(1, 2);
+            //Assert.AreEqual(expected, varCal);
+        }
+
 
     }
 }
