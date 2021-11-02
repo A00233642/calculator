617d4d534af5bac027b5cacfa3f558ecce7c63a1
A00233642 <Taiwoakintunde26@gmail.com>
2021-11-01 23:15:31 -0400

    Library division
    A new library class for division

--- calculatorLib/Class1.cs
+++ calculatorLib/Class1.cs
@@ -9,17 +9,6 @@ namespace calculatorLib
 
 
 
-        public static double Division(double a, double b)
-        {
-            if (b != 0)
-            {
-                return a / b;
-            }
-            else
-            {
-                throw new ArgumentException("Cannot divide by zero", "b");
-            }
-        }
     }
 
 }
