04d59276113327d9c48fb5a32f113ad0f72a5b84
A00233642 <Taiwoakintunde26@gmail.com>
2021-11-01 23:12:43 -0400

    Library Addition
    A new library class for addition operation

--- calculatorLib/Class1.cs
+++ calculatorLib/Class1.cs
@@ -4,10 +4,7 @@ namespace calculatorLib
 {
     public  class Class1
     {
-        public static  double Add(double a, double b)
-        {
-            return a + b;
-        }
+       
 
         public static  double Substract(double a, double b)
         {
